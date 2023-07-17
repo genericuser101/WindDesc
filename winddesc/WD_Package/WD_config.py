cbc4c93b35806a71ad735ffb98fcaf7019c6387f
genericuser101 <stepzhikharev@gmail.com>
2023-07-17 13:13:26 +0100

    bash path changes for the files.

--- winddesc/WD_Package/WD_config.py
+++ winddesc/WD_Package/WD_config.py
@@ -1,5 +1,6 @@
 class config():
     def __init__(self) -> None:
+        #The PATH is structured from where you are calling the functions.
         self.simulations_path = "../simulation/"             #ENTER SIMULATION PATH
         self.data_path = "../data/all_Dataset_V2.csv"        #ENTER DATA.CSV PATH
     
