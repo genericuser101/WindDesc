1aacb7437cccba4c6ed78a9b1210043672149afe
genericuser101 <stepzhikharev@gmail.com>
2023-07-28 09:50:26 +0100

    this is hard, I dont know what i am doing

--- WD_Package/create_turbine_files.sh
+++ WD_Package/create_turbine_files.sh
@@ -35,6 +35,6 @@ echo sbatch $simpath/$k/$k.job
 #Run the job, selection for exitcode = 0, greenlight, else, the program does not run.
 #The double exit code is slightly redundant but I want there to be a clear selection tree.
 if [ $? -eq 0 ]; then 
-  bash $simpath/$k/$k.job
+  sbatch $simpath/$k/$k.job
 fi 
 exit $exitcode
