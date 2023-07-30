95c079ffd16ff5e9029912c2a4db5158aa7b6d96
genericuser101 <stepzhikharev@gmail.com>
2023-07-30 16:36:12 +0100

    Update CSV_utility.py

--- WD_Package/CSV_utility.py
+++ WD_Package/CSV_utility.py
@@ -173,15 +173,17 @@ class CSV_Helper():
                 else:
                     if counter == int(row[3]):
                         turb_array = np.array(turb_array)
-                        
                         for i in range(counter):
                             for j in range(0, counter - i - 1):
                                 if float(turb_array[j, 0]) > float(turb_array[j + 1, 0]):
                                     turb_array[j, 5], turb_array[j + 1, 5] = turb_array[j + 1, 5], turb_array[j, 5]
                                     turb_array[j], turb_array[j + 1] = turb_array[j + 1], turb_array[j]
 
+                        prev_row = None
                         for entry in turb_array:
-                            csv_writer.writerow(entry)
+                            if entry != prev_row:  # Only write if the row is different from the previous one
+                                csv_writer.writerow(entry)
+                                prev_row = entry
 
                         counter = 0
                         turb_array = []
