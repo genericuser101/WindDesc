d8c87f5c4de9d78bc474f242629a0933ba8cfe12
genericuser101 <stepzhikharev@gmail.com>
2023-07-30 12:23:21 +0100

    ye

--- Test.py
+++ Test.py
@@ -1,5 +1,13 @@
-import WD_Package.WD_config as config1
-import WD_Package.CSV_utility
-config = config1.config()
-CSV = WD_Package.CSV_utility.CSV_Helper()
-CSV.split_data_by_turb([2, 3])
+def check_criterion(lst, criterion):
+    # The 'all()' function will check if all elements in the list meet the criterion
+    if all(element > criterion for element in lst):
+        # Your code here - this block will only execute if all elements meet the criterion
+        print("All elements in the list meet the criterion.")
+    else:
+        # This block will execute if at least one element does not meet the criterion
+        print("Not all elements in the list meet the criterion.")
+
+# Example usage:
+my_list = [10, 15, 20, 25, 30]
+criterion_value = 5
+check_criterion(my_list, criterion_value)
--- WD_Package/AL_utility.py
+++ WD_Package/AL_utility.py
@@ -27,40 +27,38 @@ class AL_Helper():
         self.CFD = CFD_utility.CFD_Helper()
         self.CSV = CSV_utility.CSV_Helper()
     
-    def rock_and_roll(self, num_iter, filename, num_turb, tolerance, method, windspeed):  
-
+    def rock_and_roll(self, num_iter, num_turb, fine_tol, abs_tol, method, windspd, winddir):  
+        
         local_sim_num = self.CFD.last_local_sim(config.simulations_path)
         local_sim_num += 1
         
         #------------------------------------TRAINING FORK-----------------------------------------#
         try:
             if method == "simple":
-                pass
+                encoder = simple_Encoder()
             elif method == "zhikh":
-                encoder = zhikh_Encoder()
+                encoder = zhikh_Encoder(fine_tol, abs_tol)
         except ValueError:
             print("Training method not recognised. Check github for currently supported.")
 
         #------------------------------------ITERATIVE LOOP----------------------------------------#
         for i in range(num_iter):
 
+            print(f"Active learning iteration: {i}, started using the {method} encoder.")
+
             #Generate coords + Train Model
             turbines, neigh = self.CFD.generate_locations()
-            trained_gp_model = self.GP.train_model(filename)
+            trained_gp_model = self.GP.train_model(config.data_path)
             refwind, refstdev = self.GP.predict_model(trained_gp_model, turbines, num_turb)
 
             #Check if there are major errors.
-            if any(refstdev) > tolerance:
-
-                if method == "simple":
-                    pass
-                elif method == "zhikh":
-                    pass
+            if any(refstdev) > abs_tol:
+                
+                encoder.project(refstdev, num_turb, turbines)
 
                 #New simulation is run on the fed-forward coordinates.
                 self.CFD.simulate(turbines, local_sim_num)
                 
-                #¬¬¬¬¬¬¬¬¬¬WE NEED TO WAIT FOR THE JOB TO BE DONE¬¬¬¬¬¬¬¬¬¬¬¬¬¬
                 #Every 20 minutes check the existance of a file. 
                 simFlag = False
                 while simFlag == False:
@@ -68,47 +66,64 @@ class AL_Helper():
                     time.sleep(1200)
 
                 #Extract newly added data and throw in the desired data file.
-                _ = str(local_sim_num).zfill(4)
-                self.CSV.extract_turbine_data(config.data_path, num_turb, windspeed, local_sim_num)
+                self.CSV.extract_turbine_data(config.data_path, num_turb, windspd, local_sim_num)
                 local_sim_num += 1
 
-                #6 Retraing the model and see if happy now
-                trained_gp_model = self.GP.train_model(filename)
+                #Retraing the model and see if happy now
+                trained_gp_model = self.GP.train_model(config.data_path)
                 refwind, refstdev = self.GP.predict_model(trained_gp_model, turbines, num_turb)
 
             else:
                 print("The model is happy, finding a new configuration.")
                  
 
-class zhikh_Encoder():
+class simple_Encoder():
     def __init__(self) -> None:
-        #------------------------Positions------------------------#
-        self.pos_tol = 0
+        pass
 
-        #-----------------------Descriptors-----------------------#
-        self.desc_tol = 0
+    def project(self, refstdev, num_turb, positions):
+        return positions
+        
 
-        self.PD_ratio = 1
+class zhikh_Encoder():
+    def __init__(self, fine_tol, abs_tol) -> None:
+
+        #------------------------Properties------------------------#
+        self.fine_tol = fine_tol
+        self.abs_tol = abs_tol
+
+        #-------------------------Utilities------------------------#
         self.TU = GP_utility.Turbine_Helper()
         self.CSV = CSV_utility.CSV_Helper()
 
-    #positions + fingerprints + neighbours
-    #check for neighbour similarity 
+    def project(self, refstdev, num_turb, positions):
 
-    def project(self, refstdev, num_turb, positions, train_data_path,):
         fingerprints = self.TU.fingerprint(positions, num_turb)
-        neigh = self.TU.nlist(positions, num_turb)
+        relevant_dataset = os.path.dirname(config.data_path) + "/" + num_turb + "_turbine_data.csv"
+        num_meet_criterion = sum(1 for element in refstdev if element > self.abs_tol)
+
+        #Case A: All errors are high.
+        if all(error > self.abs_tol for error in refstdev):
+            return positions 
         
-        #Case A: All errors are high, scenario is new, place
-        
+        #Case B: More than half of errors high.
+        if num_meet_criterion > len(refstdev) / 2:
+            self.
 
-        #Case B: More than half of errors high, tune all, place, DESC CHECK
-
-
-        #Case C: Less than half of errors high, keep the high and tune the low, place, DESK CHECK
-
-
-        #Case D: Gets sorted by the selection, model is happy.
-        
-
+        #Case C: Less than half of errors high.
+        elif num_meet_criterion < len(refstdev) / 2:
+            pass
     
+    def small_errors_to_high(self):
+        pass
+
+    def small_errors_to_medium(self):
+        pass 
+    
+    def get_X_closest(self, X, point):
+        
+
+    def distance_to_point(self, point1, point2):
+        x1, y1 = point1
+        x2, y2 = point2
+        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
