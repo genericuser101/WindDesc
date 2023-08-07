bea13718f80b83be841a88480e4b53d575faff47
genericuser101 <stepzhikharev@gmail.com>
2023-08-07 13:34:59 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -4,7 +4,7 @@ from WD_Package import WD_config
 config = WD_config.config()     
 import WD_Package.CFD_utility
 CFD = WD_Package.CFD_utility.CFD_Helper(279.00, 4000.0, 500.0, 4)
-print(CFD.last_local_sim(config.simulations_path))
+print(int(CFD.last_local_sim(config.simulations_path)) + 1 )
 #CSV.old_format_to_new("/home/eng/esugxk/storage/WindDesc/WindDesc/data/all_dataset.csv", "/home/eng/esugxk/storage/WindDesc/WindDesc/data/all_dataset_SZ.csv")
 #CSV.organise_data()
 #its currently in the new format
