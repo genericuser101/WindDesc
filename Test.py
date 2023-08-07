52a678de343639c9980093bac36d6bbbdc501580
genericuser101 <stepzhikharev@gmail.com>
2023-08-07 12:29:57 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,5 +3,5 @@ CSV = WD_Package.CSV_utility.CSV_Helper()
 from WD_Package import WD_config
 config = WD_config.config()     
 #CSV.old_format_to_new("/home/eng/esugxk/storage/WindDesc/WindDesc/data/all_dataset.csv", "/home/eng/esugxk/storage/WindDesc/WindDesc/data/all_dataset_SZ.csv")
-CSV.organise_data()
-#CSV.extract_turbine_data(config.data_path, 4, 10, 270, 4) 
+#CSV.organise_data()
+CSV.extract_turbine_data(config.data_path, 4, 10, 270, 4) 
