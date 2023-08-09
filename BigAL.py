3dab786191ea14ce16c63872a319ae3dbc5d7cdc
genericuser101 <stepzhikharev@gmail.com>
2023-08-09 15:38:56 +0100

    Update BigAL.py

--- BigAL.py
+++ BigAL.py
@@ -1,4 +1,4 @@
 from WD_Package import AL_utility
-BigAL = AL_utility.AL_Helper()
-BigAL.rock_and_roll("BigAL_test4", 4, 4, 0.05, 0.05, "simple", 10, 270)
+BigAL = AL_utility.AL_Helper("BigAL_test4")
+BigAL.rock_and_roll(4, 4, 0.05, 0.05, "simple", 10, 270)
 
